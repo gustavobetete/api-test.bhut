c6e57d09b4aaa926b17066681b907526178969da
Guuh betete <gustavobetete@hotmail.com>
2022-07-29 15:37:35 -0300

    corrigindo alguns bugs e finalizando o projeto

--- DesafioBhut/src/main/java/com/gustavobetete/desafiobhut/DesafioBhutApplication.java
+++ DesafioBhut/src/main/java/com/gustavobetete/desafiobhut/DesafioBhutApplication.java
@@ -2,8 +2,12 @@ package com.gustavobetete.desafiobhut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DesafioBhutApplication {
 
 	public static void main(String[] args) {
--- DesafioBhut/src/main/java/com/gustavobetete/desafiobhut/document/Log.java
+++ DesafioBhut/src/main/java/com/gustavobetete/desafiobhut/document/Log.java
@@ -3,9 +3,9 @@ package com.gustavobetete.desafiobhut.document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
